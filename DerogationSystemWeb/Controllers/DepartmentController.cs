adc8ff897389004e67e4477e23a44e3d59bddf4e
alex <a.korneyko@gmail.com>
2020-05-08 16:59:06 +0200

    Add project files.

---
+++ DerogationSystemWeb/Controllers/DepartmentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DerogationSystemWeb.Model.Configs;
+using DerogationSystemWeb.Model.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DerogationSystemWeb.Controllers
+{
+    [ApiController]
+    [Route("api/departments")]
+    public class DepartmentController : Controller
+    {
+        private readonly ApplicationContext _dataBase;
+
+        public DepartmentController(ApplicationContext dataBase)
+        {
+            this._dataBase = dataBase;
+        }
+
+        [HttpGet]
+        public IEnumerable<Department> Get()
+        {
+            var departments = _dataBase.Departments.ToList();
+
+            return departments;
+        }
+    }
+}
---
+++ DerogationSystemWeb/Model/Configs/ApplicationContext.cs
@@ -0,0 +1,44 @@
+﻿using DerogationSystemWeb.Model.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace DerogationSystemWeb.Model.Configs
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>()
+                .Property(department => department.Mandatory).HasConversion(
+                    v => Conversions.ToInteger(v).ToString(),
+                    v => Conversions.ToBoolean(Conversions.ToInteger(v)));
+
+            modelBuilder.Entity<Department>()
+                .Property(department => department.LtimeAccess).HasConversion(
+                    v => Conversions.ToInteger(v).ToString(),
+                    v => Conversions.ToBoolean(Conversions.ToInteger(v)));
+
+            modelBuilder.Entity<Department>()
+                .Property(department => department.DCostAccess).HasConversion(
+                    v => Conversions.ToInteger(v).ToString(),
+                    v => Conversions.ToBoolean(Conversions.ToInteger(v)));
+
+            modelBuilder.Entity<Department>()
+                .Property(department => department.ToBeAdded).HasConversion(
+                    v => Conversions.ToInteger(v).ToString(),
+                    v => Conversions.ToBoolean(Conversions.ToInteger(v)));
+
+            modelBuilder.Entity<Department>()
+                .Property(department => department.OnlyMail).HasConversion(
+                    v => Conversions.ToInteger(v).ToString(),
+                    v => Conversions.ToBoolean(Conversions.ToInteger(v)));
+        }
+    }
+}
